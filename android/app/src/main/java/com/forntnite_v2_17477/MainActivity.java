3203258eb09a39bb852535b5327234b6ae80f15f
Crowdbotics <team@crowdbotics.com>
2020-05-27 23:38:09 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/forntnite_v2_17477/MainActivity.java
@@ -0,0 +1,15 @@
+package com.forntnite_v2_17477;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "forntnite_v2_17477";
+    }
+}
